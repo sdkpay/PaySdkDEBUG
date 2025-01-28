f26ead9623ff5863d4077ddd8825b6290504fdae
Mikhail Seregin <m.seregin@kaycom.ru>
2025-01-28 15:04:42 +0500

    2.5.0

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SPaySdkDEBUG",
+    defaultLocalization: "ru",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "SPaySdkDEBUG",
+            targets: ["SPaySdkDEBUG"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        .binaryTarget(name: "SPaySdkDEBUG",
+                      path: "SPaySdkDEBUG.xcframework"),
+        .target(name: "SPaySdkDEBUGPackage",
+                dependencies: [
+                    .target(name: "SPaySdkDEBUG")
+                ],
+                path: "./"
+    )
+    ]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# PaySDKDebug
+
+A description of this package.
